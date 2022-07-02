8be9562ed2f5548b02535dd6736bc0df4153ce0c
dmateons <dmateons@gmail.com>
2022-07-02 03:54:41 -0500

    dash project

---
+++ components/sampledf/model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#df_costos = pd.read_csv('./data/dfsample/costos.csv')
+
+#df_opsales = pd.read_csv('./data/dfsample/opsales.csv')
+
+#df_markers = pd.read_csv('./data/dfsample/markers.csv')
+
+# dataset de prueba para el mapa
+datatest ={
+        '#':['1', '2', '3', '4'], 
+        'MES': ['FEBRERO', 'DICIEMBRE', 'MARZO', 'JUNIO'], 
+        'PLACA':['ABC12x', 'BCD452', 'PCL124','PER85Y'], 
+        'KM/h':['186', '145', '123','112'], 
+        'DEPARTAMENTO': ['SANTANDER', 'ANTIOQUIA', 'CUNDINAMARCA', 'BOYACA'],
+        'COUNT':[99, 900, 9000,900000], 
+        'COD_DPTO':['68', '05', '25','15']} 
+  
+df_maptest = pd.DataFrame.from_dict(datatest)  
---
+++ pages/home.py
@@ -0,0 +1,67 @@
+from logging import _STYLES
+import dash
+from dash import html , dcc
+import dash_bootstrap_components as dbc
+from dash_labs.plugins import register_page
+
+from components.table.table import table
+from components.maps.mapcol_departamentos import mapcol_departamentos
+from components.sampledf.model import df_maptest
+
+mapa_colombia_departamentos = mapcol_departamentos('Mapa Trafico en la Bogotá D.C', 'div_municipios_fig2', df_maptest)
+params1 = {
+            'title': 'TOP Velocidades', 
+            'description': 'Tabla con los vehiculos mas rapidos',
+            'columns': [ '#', 'MES', 'PLACA', 'KM/h']
+}
+tabla_datos_departamentos = table(df_maptest,params1)
+
+
+register_page(__name__, path="")
+
+layout = dbc.Container(
+    [
+        html.Div([
+            
+            html.Div(children=[
+                html.H5("MES"),
+                dbc.Row([
+                    dcc.RangeSlider(
+                        id='range_slider',
+                        min=0,
+                        max=12,
+                        step=1,
+                        value=[3, 5]
+                        )
+                ]),
+                
+                dcc.Dropdown(df_maptest.DEPARTAMENTO.unique(),
+                 id='memory-countries',
+                 multi=True),
+                
+                dbc.Row([
+                    dbc.Col([
+                        html.Div([
+                            mapa_colombia_departamentos.display()  
+                        ],id="row_map")   
+                    ])
+                ], ), 
+                
+            ],className= "card", style={'width': '60%', 'display': 'inline-block'}
+            )
+                 
+            ,
+                
+            html.Div(children=[
+                
+                html.Div([
+                    tabla_datos_departamentos.display()
+                ],id="row_tabla")  
+                
+            ],className= "card", style={'width': '40%', 'display': 'inline-block'})
+            
+                
+        ],className= "card", style={'display': 'flex', 'flex-direction': 'row'}
+        )
+    ]
+)
