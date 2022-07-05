eac23df33db3afb4bee980ad481ee3faa98f50c1
dmateons <dmateons@gmail.com>
2022-07-04 21:09:27 -0500

    test push

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from dash import Input, Output
 
 request_path_prefix = "/"
 
-# #only for workspace in DS4A
-# workspace_user = os.getenv('JUPYTERHUB_USER')  # Get DS4A Workspace user name
-# if workspace_user:
-#     request_path_prefix = '/user/' + workspace_user + '/proxy/8050/'
-
-
 # Dash instance declaration
 app = dash.Dash(__name__, plugins=[dl.plugins.pages], requests_pathname_prefix=request_path_prefix,
                 external_stylesheets=[dbc.themes.FLATLY], prevent_initial_callbacks=True)
